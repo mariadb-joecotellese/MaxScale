bf80b06f627f590ee49082fe264b1e694a59819d
Niclas Antti <jnantti@gmail.com>
2023-11-27 10:14:53 +0200

    MXS-4791 class InmemoryStorage

---
+++ server/modules/filter/wcar/wcarinmemorystorage.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2024 MariaDB plc
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of MariaDB plc
+ */
+#include "wcarinmemorystorage.hh"
+#include <maxbase/assert.hh>
+#include <sstream>
+
+void InmemoryStorage::add_query_event(QueryEvent&& qevent)
+{
+    auto hash = std::hash<std::string>{}(qevent.canonical);
+    auto can_id = next_can_id();
+    CanonicalEntry entry{can_id, std::move(qevent.canonical)};
+
+    auto p = m_canonicals.try_emplace(hash, std::move(entry));
+    if (p.second == false)
+    {
+        if (p.first->second.canonical != entry.canonical)
+        {
+            // TODO potentially serious, although very unlikely error. Decide what to do.
+            MXB_SERROR("Hash collision. In storage:'" << p.first->first << "' attempt to insert '"
+                                                      << entry.canonical);
+        }
+    }
+
+    m_canonical_lookup.emplace(can_id, p.first);
+    m_events.emplace_back(next_event_id(), can_id, std::move(qevent.canonical_args));
+}
+
+Storage::Iterator InmemoryStorage::begin()
+{
+    QueryEvent qe{"", maxsimd::CanonicalArgs{}, m_read_event_idx};
+    return Storage::Iterator(this, next_event(std::move(qe)));
+}
+
+Storage::Iterator InmemoryStorage::end() const
+{
+    return Storage::Iterator(nullptr, QueryEvent {});
+}
+
+size_t InmemoryStorage::num_unread() const
+{
+    return m_events.size();
+}
+
+QueryEvent InmemoryStorage::next_event(const QueryEvent& event)
+{
+    if (m_events.empty())
+    {
+        return QueryEvent{};
+    }
+
+    auto& cap_event = m_events.front();
+    auto lookup_ite = m_canonical_lookup.find(cap_event.can_id);
+    auto& canon_ite = lookup_ite->second;
+
+    QueryEvent ret{std::move(canon_ite->second.canonical), std::move(cap_event.args), m_read_event_idx};
+
+    ++m_read_event_idx;
+    m_canonical_lookup.erase(lookup_ite);
+    m_canonicals.erase(canon_ite);
+    m_events.pop_front();
+
+    return ret;
+}
---
+++ server/modules/filter/wcar/wcarinmemorystorage.hh
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2024 MariaDB plc
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of MariaDB plc
+ */
+#pragma once
+
+#include "wcarstorage.hh"
+#include <map>
+#include <unordered_map>
+#include <deque>
+
+class InmemoryStorage final : public Storage
+{
+public:
+    void     add_query_event(QueryEvent&& qevent) override;
+    Iterator begin() override;
+    Iterator end() const override;
+    size_t   num_unread() const override;
+
+private:
+    QueryEvent next_event(const QueryEvent& event) override;
+
+    struct CanonicalEntry
+    {
+        size_t      can_id;
+        std::string canonical;
+        CanonicalEntry(size_t can_id, std::string&& canonical)
+            : can_id(can_id)
+            , canonical(std::move(canonical))
+        {
+        }
+    };
+
+    // <canonical hash, CanonicalEntry>
+    // The reason for using a map is so that the canonical can be moved in and out
+    // from the container. Could use an unordered_set: given an iterator, break the
+    // rules and const_cast the element, extract canonical and erase using the iterator.
+    using Canonicals = std::map<size_t, CanonicalEntry>;
+
+    // Reverse lookup into above map using can_id. Not const_iterator because
+    // the element is moved from, before it is erased.
+    using CanonicalLookup = std::unordered_map<size_t, Canonicals::iterator>;
+
+    struct CaptureEvent
+    {
+        size_t event_id;
+        size_t can_id;
+
+        maxsimd::CanonicalArgs args;
+
+        CaptureEvent(size_t event_id, size_t can_id, maxsimd::CanonicalArgs&& args)
+            : event_id(event_id)
+            , can_id(can_id)
+            , args(std::move(args))
+        {
+        }
+    };
+
+    // capture events
+    using CaptureEvents = std::deque<CaptureEvent>;
+
+    Canonicals      m_canonicals;
+    CanonicalLookup m_canonical_lookup;
+    CaptureEvents   m_events;
+    size_t          m_read_event_idx = 0;
+};
