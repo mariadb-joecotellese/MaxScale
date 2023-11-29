8b5a373cd2187a14cad7eae9c168824b0fc86d17
Niclas Antti <jnantti@gmail.com>
2023-11-29 12:46:13 +0200

    MXS-4791 Send capture events to the recorder

--- server/modules/filter/wcar/wcarfiltersession.cc
+++ server/modules/filter/wcar/wcarfiltersession.cc
@@ -5,6 +5,8 @@
  */
 
 #include "wcarfiltersession.hh"
+#include "wcarfilter.hh"
+#include <maxsimd/canonical.hh>
 
 WcarFilterSession::WcarFilterSession(MXS_SESSION* pSession, SERVICE* pService, const WcarFilter* pFilter)
     : maxscale::FilterSession(pSession, pService)
@@ -21,4 +23,24 @@ WcarFilterSession* WcarFilterSession::create(MXS_SESSION* pSession, SERVICE* pSe
                                              const WcarFilter* pFilter)
 {
     return new WcarFilterSession(pSession, pService, pFilter);
+}
+
+bool WcarFilterSession::routeQuery(GWBUF&& buffer)
+{
+    m_query_event.canonical= parser().get_sql(buffer);
+    m_query_event.canonical_args.clear();
+    maxsimd::get_canonical(&m_query_event.canonical, &m_query_event.canonical_args);
+
+    return mxs::FilterSession::routeQuery(std::move(buffer));
+}
+
+bool WcarFilterSession::clientReply(GWBUF&& buffer,
+                                    const maxscale::ReplyRoute& down,
+                                    const maxscale::Reply& reply)
+{
+    auto pWorker = mxs::RoutingWorker::get_current();
+    auto pShared_data = m_filter.recorder().get_shared_data_by_index(pWorker->index());
+    pShared_data->send_update(m_query_event);
+
+    return mxs::FilterSession::clientReply(std::move(buffer), down, reply);
 }
--- server/modules/filter/wcar/wcarfiltersession.hh
+++ server/modules/filter/wcar/wcarfiltersession.hh
@@ -6,23 +6,27 @@
 #pragma once
 
 #include "wcardefs.hh"
+#include "wcarstorage.hh"
 #include <maxscale/filter.hh>
 
 class WcarFilter;
 
-class WcarFilterSession : public maxscale::FilterSession
+class WcarFilterSession final : public maxscale::FilterSession
 {
 public:
     ~WcarFilterSession();
 
     static WcarFilterSession* create(MXS_SESSION* pSession, SERVICE* pService, const WcarFilter* pFilter);
 
+    bool routeQuery(GWBUF&& buffer) override;
+    bool clientReply(GWBUF&& buffer, const mxs::ReplyRoute& down, const mxs::Reply& reply) override;
+
 private:
     WcarFilterSession(MXS_SESSION* pSession, SERVICE* pService, const WcarFilter* pFilter);
-
-    WcarFilterSession(const WcarFilterSession&);
-    WcarFilterSession& operator=(const WcarFilterSession&);
+    WcarFilterSession(WcarFilterSession&&) = delete;
 
 private:
     const WcarFilter& m_filter;
+
+    QueryEvent m_query_event;
 };
