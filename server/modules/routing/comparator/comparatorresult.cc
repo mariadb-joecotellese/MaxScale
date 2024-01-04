228ba9fee35c7cdb7056b3453da8acbefd7892e0
Johan Wikman <johan.wikman@mariadb.com>
2024-01-04 10:31:04 +0200

    MXS-4761 Add missing comparatorresult.cc

---
+++ server/modules/routing/comparator/comparatorresult.cc
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024 MariaDB plc
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of MariaDB plc
+ */
+
+#include "comparatorresult.hh"
+#include "comparatorbackend.hh"
+
+/**
+ * ComparatorResult
+ */
+ComparatorResult::~ComparatorResult()
+{
+}
+
+void ComparatorResult::close(const mxs::Reply& reply)
+{
+    mxb_assert(!closed());
+    m_reply = reply;
+    m_end = Clock::now();
+}
+
+
+/**
+ * ComparatorMainResult
+ */
+ComparatorMainResult::ComparatorMainResult(ComparatorMainBackend* pBackend,
+                                           const std::string& sql,
+                                           uint8_t command)
+    : ComparatorResult(pBackend)
+    , m_sql(sql)
+    , m_command(command)
+{
+}
+
+void ComparatorMainResult::close(const mxs::Reply& reply)
+{
+    ComparatorResult::close(reply);
+
+    // A dependent may end up removing itself.
+    auto dependents = m_dependents;
+
+    for (ComparatorOtherResult* pDependent : dependents)
+    {
+        pDependent->main_was_closed();
+    }
+}
+
+/**
+ * ComparatorOtherResult
+ */
+
+ComparatorOtherResult::ComparatorOtherResult(ComparatorOtherBackend* pBackend,
+                                             std::shared_ptr<ComparatorMainResult> sMain_result)
+    : ComparatorResult(pBackend)
+    , m_sMain_result(sMain_result)
+{
+    m_sMain_result->add_dependent(this);
+}
+
+ComparatorOtherResult::~ComparatorOtherResult()
+{
+    m_sMain_result->remove_dependent(this);
+}
+
+
+void ComparatorOtherResult::close(const mxs::Reply& reply)
+{
+    ComparatorResult::close(reply);
+
+    if (m_sMain_result->closed())
+    {
+        static_cast<ComparatorOtherBackend&>(backend()).ready(*this);
+    }
+}
+
+void ComparatorOtherResult::main_was_closed()
+{
+    if (closed())
+    {
+        static_cast<ComparatorOtherBackend&>(backend()).ready(*this);
+    }
+}
