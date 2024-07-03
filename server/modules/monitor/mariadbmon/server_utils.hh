ebe6e1345f553cbabbd194aeaeed84c44b3de522
Esa Korhonen <esa.korhonen@mariadb.com>
2024-07-03 10:37:51 +0300

    MXS-5047 Perform failover on master write test failure
    Performs a failover and sets the old master to maintenance.

--- server/modules/monitor/mariadbmon/server_utils.hh
+++ server/modules/monitor/mariadbmon/server_utils.hh
@@ -161,7 +161,7 @@ enum class OperationType
 
 enum class OpStart {MANUAL, AUTO};
 enum class SwitchoverType {NORMAL, AUTO, FORCE};
-enum class FailoverType {SAFE, ALLOW_TRX_LOSS};
+enum class FailoverType {SAFE, ALLOW_TRX_LOSS, WRITES_HANGED};
 
 class GeneralOpData
 {
